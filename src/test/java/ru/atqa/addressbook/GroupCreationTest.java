2b794e4e27244f798fa6f924228ac27a30882baf
Anton Vlasov <nocsland@gmail.com>
2021-01-16 10:33:29 +0400

    авторизация вынесена в Before

--- src/test/java/ru/atqa/addressbook/GroupCreationTest.java
+++ src/test/java/ru/atqa/addressbook/GroupCreationTest.java
@@ -16,14 +16,15 @@ public class GroupCreationTest {
     public void start() throws Exception {
         driver = new OperaDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void GroupCreationTest() {
         driver.get("http://localhost/addressbook/group.php");
         driver.findElement(By.name("user")).sendKeys("admin");
         driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.xpath("//*[@id=\"LoginForm\"]/input[3]")).click();
+    }
+
+    @Test
+    public void testGroupCreation() {
+
         driver.findElement(By.xpath("//*[@id=\"nav\"]/ul/li[3]/a")).click();
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
