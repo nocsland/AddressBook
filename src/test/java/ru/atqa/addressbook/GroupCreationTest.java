29997ba054c3b842b594c4fd657c9cad2bb83842
Anton Vlasov <nocsland@gmail.com>
2021-01-16 10:13:04 +0400

    Добавлен тест "Создание новой группы"

---
+++ src/test/java/ru/atqa/addressbook/GroupCreationTest.java
@@ -0,0 +1,53 @@
+package ru.atqa.addressbook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class GroupCreationTest {
+    private WebDriver driver;
+
+    @Before
+    public void start() throws Exception {
+        driver = new OperaDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void GroupCreationTest() {
+        driver.get("http://localhost/addressbook/group.php");
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.xpath("//*[@id=\"LoginForm\"]/input[3]")).click();
+        driver.findElement(By.xpath("//*[@id=\"nav\"]/ul/li[3]/a")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).clear();
+        driver.findElement(By.name("group_name")).sendKeys("test1");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).clear();
+        driver.findElement(By.name("group_header")).sendKeys("test2");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).clear();
+        driver.findElement(By.name("group_footer")).sendKeys("test3");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+
+
+
+
+
